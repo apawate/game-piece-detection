80c0665b4775bf5a55a1945fe1df09cda5b7feb8
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2023-01-14 17:22:53 -0800

    Added some blue/yellow differences

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ while(capture):
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	lower_blue = np.array([110,50,50])
 	upper_blue = np.array([130,255,255])
+	lower_yellow = np.array([20, 100, 100])
+	upper_yellow = np.array([30, 255, 255])
 
-    # Creates a mask showing all "blue" images in the frame
-	mask = cv2.inRange(hsv, lower_blue, upper_blue)
-	res = cv2.bitwise_and(frame,frame, mask= mask)
+
+
+    # Creates a mask showing all "blue" objects and one showing all "yellow" objects in the frame
+	mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+	mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+	res_blue = cv2.bitwise_and(frame,frame, mask= mask_blue)
+	res_yellow = cv2.bitwise_and(frame, frame, mask= mask_yellow)
 	cv2.imshow('frame',frame)
-	cv2.imshow('mask',mask)
-	cv2.imshow('res',res)
+	cv2.imshow('mask',mask_yellow)
+	cv2.imshow('res',res_yellow)
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
 		break
